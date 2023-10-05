50a6b5f327ede54aaf3868fbcd8f82face1fb9ef
emancho <emancho@umd.edu>
2023-10-04 18:24:39 -0700

    completed creative page outline.

--- persona-website/src/Pages/CreativePage.js
+++ persona-website/src/Pages/CreativePage.js
@@ -1,11 +1,5 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 // --- Material UI
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
 ///--- Bottom Navigation
 import BadgeIcon from '@mui/icons-material/Badge';
 import BottomNavigation from '@mui/material/BottomNavigation';
@@ -31,7 +25,7 @@ return (
   <Box sx={{pb: 7}}>
     <CssBaseline />
     <div>
-      
+      <CountdownPage/>
     </div>
     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
       <BottomNavigation     
@@ -63,31 +57,48 @@ return (
 );
 }
 
-function VinylList() {
+function CountdownPage() {
+  const [countdown, setCountdown] = useState(null);
+
+  useEffect(() => {
+    // Set the target date (November 1, 2023)
+    const targetDate = new Date('2023-11-01T00:00:00'); // Assuming midnight of November 1
+
+    // Set the initial date (October 4, 2023)
+    const initialDate = new Date('2023-10-04T00:00:00'); // Assuming midnight of October 4
+
+    // Calculate the time remaining in milliseconds
+    const timeRemaining = targetDate - initialDate;
+
+    // Update the countdown every second
+    const interval = setInterval(() => {
+      const now = new Date();
+      const timeLeft = targetDate - now;
+
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+        setCountdown('Countdown expired');
+      } else {
+        const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        setCountdown(
+          `${daysLeft} days ${hoursLeft} hours ${minutesLeft} minutes ${secondsLeft} seconds`
+        );
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <ImageList sx={{ width: 500, height: 450 }}>
-      <ImageListItem key="Subheader" cols={1}>
-        <ListSubheader component="div">Contact Me</ListSubheader>
-      </ImageListItem>
-        <ImageListItem key={"Test"}>
-          <img
-            src={`https://pngimg.com/uploads/vinyl/vinyl_PNG66.png`}
-            alt={'#1'}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={'Edd Man'}
-            subtitle={'I wanna be an artist'}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-    </ImageList>
+    <div className="countdown-page">
+      <h1>Countdown: {countdown}</h1>
+    </div>
   );
 }
 

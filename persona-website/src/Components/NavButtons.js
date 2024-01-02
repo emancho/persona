56ac69cee4aede13ce772c281c8d4d36ceac5182
emancho <emancho@umd.edu>
2024-01-01 18:26:05 -0800

    fixing NavButtons for homePage

--- persona-website/src/Components/NavButtons.js
+++ persona-website/src/Components/NavButtons.js
@@ -1,39 +1,19 @@
 import React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
+import { IconButton, Stack } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-function NavButtons({buttonData}) {
-    
-    return (
-        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-          {buttonData.map(({ icon, link }, index) => (
-            <IconButton key={index} onClick={() => window.location.href = link}>
-              {React.createElement(icon, { fontSize: 'large' })}
-            </IconButton>
-          ))}
-        </Stack>
-      );
+function NavButtons({ buttonData }) {
+  const navigate = useNavigate();
 
-
-
-
-
-
-
-
-
-
-
-
-//     const iconSize = 'large';
-
-//   return (
-//     <Stack direction="row" spacing={2} justifyContent="center" alignItems="center" >
-//       <IconButton><SkipPrevIcon fontSize={iconSize} /></IconButton>
-//       <IconButton><PlayArrowIcon fontSize={iconSize} /></IconButton>
-//       <IconButton><SkipNextIcon fontSize={iconSize} /></IconButton>
-//     </Stack>
-//   );
+  return (
+    <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+      {buttonData.map(({ icon, link }, index) => (
+        <IconButton key={index} onClick={() => navigate(link)}>
+          {React.createElement(icon, { fontSize: 'large' })}
+        </IconButton>
+      ))}
+    </Stack>
+  );
 }
 
 export default NavButtons;

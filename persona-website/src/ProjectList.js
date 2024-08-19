bef7900af213b8b2f92abac68ed94606901c3388
emancho <emancho@umd.edu>
2024-08-19 11:06:57 -0700

    adding updates before deploying webpage update

--- persona-website/src/ProjectList.js
+++ persona-website/src/ProjectList.js
@@ -21,13 +21,13 @@ export const listOfProjects = [
         projTitle:'The Gals Just Want to Dance',
         projDesc: 'This project is a music album of tracks that women wanna dance to. The album is planned to have 6 or more songs and will be inspired by multiple genres (Funk, Disco, R&B, House, Etc). Album cover is based on a true story and is one of my inspirations behind making this album.',
         projAction:'POPUP',
-        projResponse: `Author notes: I don't have a release date, so please be patient. I have a vision of what I want the album to be like and am working on making it a reality!! Just wait on it :D`
+        projResponse: `Author notes: I don't have a release date, so please be patient. I have a vision of what I want the album to be like and working on making it a reality!! Just wait on it while I learn Music Theory :D`
     },
     {
         id:3,
         img: `${process.env.PUBLIC_URL}/images/techbro_drac.png`,
         projTitle:'Tech Bro Dracula',
-        projDesc: `This project is a script I'm working on using Dracula's public domain usage. The premise is that Dracula is struggling to survive in a world filled with modern-day technology so he attempts to adapt and not die from stravation.`,
+        projDesc: `This project is a script I'm working on using Dracula's public domain. The premise is that Dracula is struggling to survive in a world filled with modern-day technology and apps, making it challenging to suck blood while staying incognito. To avoid dying from starvation, Dracula reluctantly embraces the very thing he hates—technology—and transforms into a Tech Bro to satisfy his hunger.`,
         projAction:'POPUP',
         projResponse:`Author notes: Currently working on the rough draft pilot script. Will post when completed...15 pages done so far!`
     },

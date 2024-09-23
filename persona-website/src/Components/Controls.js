319c82e4607814349d8fc8c72738c6077ef79a7d
emancho <emancho@umd.edu>
2024-09-23 11:28:56 -0700

    added text to buttons, modified button placement and increased size of play/pause button. Next target: Modifying playback button implementation

--- persona-website/src/Components/Controls.js
+++ persona-website/src/Components/Controls.js
@@ -7,12 +7,9 @@ import {
 } from 'react';
 //== Playback icons
 import {
-IoPlaySkipBackSharp,
-IoPlaySkipForwardSharp,
 IoPlaySharp,
 IoPauseSharp,
 } from 'react-icons/io5';
-
 import { CgPlayForwards } from "react-icons/cg";
 import { CgPlayBackwards } from "react-icons/cg";
 
@@ -28,7 +25,7 @@ import {
   VOLUME_BUTTON_SIZE
 } from '../Constants'
 //== Components
-import {Box, Grid} from '@mui/material';
+import {Box, Grid, Typography} from '@mui/material';
 
 /*
 Component Description:
@@ -113,63 +110,76 @@ useEffect(() => {
     }
   }, [volume, audioRef, muteVolume]);
 
-return (
-  <Grid container direction="column" spacing={2} justifyContent="center">
-    <Grid item>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        {/* Previous Button */}
-        <Box mr={5}>
-          <button onClick={handlePrev}>
-            <CgPlayBackwards size={PLAYBACK_BUTTON_SIZE} />
-          </button>
-        </Box>
-
-        {/* Play/Pause Button */}
-        <Box mr={5}>
-          <button onClick={togglePlayPause}>
-            {isPlaying ? (
-              <IoPauseSharp size={PLAYBACK_BUTTON_SIZE} />
-            ) : (
-              <IoPlaySharp size={PLAYBACK_BUTTON_SIZE} />
-            )}
-          </button>
+  return (
+    <Grid container direction="column" spacing={1} justifyContent="center">
+      <Grid item>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          
+          {/* Previous Button */}
+          <Box mr={5} display="flex" flexDirection="column" alignItems="center">
+            <Typography variant="body1" mb={1} align="center">
+              Previous
+            </Typography>
+            <button onClick={handlePrev}>
+              <CgPlayBackwards size={PLAYBACK_BUTTON_SIZE} />
+            </button>
+            <Typography variant="body1" mt={1} align="center">
+            Episode
+            </Typography>
           </Box>
 
-        {/* Next Button */}
-        <Box>
-          <button onClick={handleNext}>
-            <CgPlayForwards size={PLAYBACK_BUTTON_SIZE} />
-          </button>
-        </Box>
-      </Box>
-    </Grid>
+          {/* Play/Pause Button */}
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <button onClick={togglePlayPause}>
+              {isPlaying ? (
+                <IoPauseSharp size={PLAYBACK_BUTTON_SIZE + 20} />
+              ) : (
+                <IoPlaySharp size={PLAYBACK_BUTTON_SIZE + 20} />
+              )}
+            </button>
+          </Box>
 
-    {/* Mute Button and Volume Slider */}
-    <Grid item xs={12} style={{ textAlign: 'center', marginTop: '10px' }}>
-      <button onClick={() => setMuteVolume((prev) => !prev)}>
-        {muteVolume || volume < 5 ? (
-          <IoMdVolumeOff size={VOLUME_BUTTON_SIZE} />
-        ) : volume < 40 ? (
-          <IoMdVolumeLow size={VOLUME_BUTTON_SIZE} />
-        ) : (
-          <IoMdVolumeHigh size={VOLUME_BUTTON_SIZE} />
-        )}
-      </button>
-      <input
-        type="range"
-        min={0}
-        max={100}
-        value={volume}
-        onChange={(e) => setVolume(e.target.value)}
-        style={{
-          background: `linear-gradient(to right, #f50 ${volume}%, #ccc ${volume}%)`,
-          marginLeft: '10px',
-          verticalAlign: 'middle',
-        }}
-      />
+          {/* Next Button */}
+          <Box ml={5} display="flex" flexDirection="column" alignItems="center">
+            <Typography variant="body1" mb={1} align="center">
+              Next
+            </Typography>
+            <button onClick={handleNext}>
+              <CgPlayForwards size={PLAYBACK_BUTTON_SIZE} />
+            </button>
+            <Typography variant="body1" mt={1} align="center">
+              Episode
+            </Typography>
+          </Box>
+        </Box>
+      </Grid>
+
+      {/* Mute Button and Volume Slider */}
+      <Grid item xs={12} style={{ textAlign: 'center', marginTop: '10px' }}>
+        <button onClick={() => setMuteVolume((prev) => !prev)}>
+          {muteVolume || volume < 5 ? (
+            <IoMdVolumeOff size={VOLUME_BUTTON_SIZE} />
+          ) : volume < 40 ? (
+            <IoMdVolumeLow size={VOLUME_BUTTON_SIZE} />
+          ) : (
+            <IoMdVolumeHigh size={VOLUME_BUTTON_SIZE} />
+          )}
+        </button>
+        <input
+          type="range"
+          min={0}
+          max={100}
+          value={volume}
+          onChange={(e) => setVolume(e.target.value)}
+          style={{
+            background: `linear-gradient(to right, #f50 ${volume}%, #ccc ${volume}%)`,
+            marginLeft: '10px',
+            verticalAlign: 'middle',
+          }}
+        />
+      </Grid>
     </Grid>
-  </Grid>
-);
+    );
 };
 
 export default Controls;
